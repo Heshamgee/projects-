1cbfadfa2402fb905ed63c11f2c44b94990f1bbb
hesham amer <heshamamer1219@gmail.com>
2024-02-06 17:17:44 +0200

    initilizing jupytr for python

--- python/functions.py
+++ python/functions.py
@@ -1,24 +1,29 @@
 #function as a tutorial 
 """function is a block of code that can use it when we call"""
-"""
-calculation_to_units = 24
-name_of_unit = "hours"
 
-def days_to_units(num_of_days , custom_message):
- print (f"{num_of_days} days = {num_of_days * calculation_to_units}{name_of_unit}")
- print(custom_message)
- 
-days_to_units(55, "awesome!")
-days_to_units(544, "good!")
-"""
 #my code is a function calculats the number of hours in the day with variables days and printing message
 def hours_on_days(no_of_days, message):
-    calculation = no_of_days * 24 
-    print(f"The number of hours on {no_of_days} days is = {calculation}")
-    print(message)
-
+    if no_of_days > 0 :
+         calculation = no_of_days * 24 
+         print(f"The number of hours on {no_of_days} days is = {calculation}")
+         print(message)
+           
+    else: 
+        return "u intered a negative value "
 # Taking user input
 user_input=int(input("inter the number of days ")) 
 
 # Calling the function  
 hours_on_days(user_input, "thanks for using our function")
+
+def validate_and_execute():
+    #using a function and his prameter as input in one line
+if user_input.isdigit() :
+    
+    #casting = is convert one data type  to other data type variable  
+    user_input_number =int(user_input)
+
+    calculated_value =days_to_units(user_input_number)
+    print(calculated_value)
+else :
+    print("your input is not a number ,dont ruin my program")
